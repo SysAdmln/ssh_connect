0bc3241b1273d360525265d5072cbd574df8c770
SysAdmin <sys@dmin.pro>
2022-08-31 18:00:22 +0300

    fix

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-help(){
-    cat<<__EOF__
-usage: ssh_connect.sh local_IP remote_IP
 
-    Examples:
-	ssh_connect.sh 192.168.31.3 192.168.32.10
-__EOF__
-}
+if [ $1 == help ];then
 
+	echo "usage: ssh_connect.sh local_IP remote_IP
+
+	Examples:
+        ssh_connect.sh 192.168.31.3 192.168.32.10"
+
+else
+
+echo $1 
+echo $2
 fping -q -r 1 $1
 if [ $? == 0 ];then
-        echo"Local connect"
+        echo "Local connect"
    ssh -X arturius@$1
 else
         echo "ZeroTier connect"
    ssh -X arturius@$2
 fi
+fi
+
